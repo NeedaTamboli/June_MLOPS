f5ba79dbdb4af2a8af11ff2b0a73ba5a18581e37
NeedaTamboli <114369431+NeedaTamboli@users.noreply.github.com>
2024-07-07 10:08:01 +0530

    Update a.py

--- a.py
+++ a.py
@@ -3,3 +3,5 @@ print("Hello Brother!!!!")
 print("I want to learn MLOPS github, pycharm and commit")
 
 print("Creating different versions by doing commit it is nothing but ctrl + s.")
+
+print("I am changing my code in Github and then I will fetch it.")
